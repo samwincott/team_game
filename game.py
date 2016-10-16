89afebd82a10d60f089f3a57a8279bf51612b5b1
ollie1212 <ollie.gunnell@gmail.com>
2016-10-16 17:04:56 +0100

    few bugs

--- game.py
+++ game.py
@@ -80,8 +80,8 @@ def adding_a_memory(timeslot):
     """This will be called when the player can remember something from the night before
     This should be called like adding_a_memory('12pm-1am')"""
 
-        memory.append(timeslot)
-        return
+    memory.append(timeslot)
+    return
 
 
     print("You cannot take that.")      
@@ -91,11 +91,11 @@ def print_memory(current_memory):
     This should be called with the memory inventory in player.py like print_memory(memory)."""
 
     for x in current_memory:
-        print("You know what happened between " + x["id"] ".")
+        print("You know what happened between " + x["id"] + ".")
 
 def what_happened_between(timeslot):
     """This will print what happened at a specific time slot, given the time"""        
-
+    
 
 def print_room(room):
     """This function takes a room as an input and nicely displays its name

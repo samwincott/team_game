dd6919a9529a4edd5e826d9795e7b392a7ce12ac
JoeAJLaw <joseph.a.j.law@gmail.com>
2016-10-16 21:34:11 +0100

    This contains all the actual people we need

---
+++ friend.py
@@ -0,0 +1,79 @@
+from items import *
+from map import rooms
+
+
+
+friend_Rick = {
+
+    "name": "Rick"
+    
+    "items": []
+
+    "Triggered":
+    """I only re-call us being with Morty last night, might want to ask him mate”. """
+
+    "memory": []    
+
+}
+
+friend_Morty = {
+    "name": "Summer"
+
+    "items": []
+
+    "Triggered":
+    """Heeeeey!! Whats up man!? From what I recall you guys were pretty crazy last night. Oh you look like you’ve lost your jacket mate? Well from the portion I remember you had it in The Taf, might want to go check there. """
+
+    "memory": []
+}
+
+friend_Summer = {
+
+    "items": []
+
+    "Triggered":
+    """For some strange reason you threw most people’s keys into a kebab box and starting hiding it around the house, saying the kebab knight will protect our keys! Which… by the way was not fun when you put it in the bin… which is where I last left it """
+
+    "memory": []
+
+}
+
+friend_Bar_Staff = {
+
+    "name": "Bar"
+
+    "Triggered":
+    """OH! ITS YOU!? THE LEGEND WHO SMASHED LIKE 15 Jaegar Bombs in 5 mins! You were pretty impressive, can’t believe you were still partying after all that! Oh and your jacket is under the pool table """
+
+    "memory" : []
+
+}
+
+friend_Club_Rep = {
+
+    "name": "Club"
+
+    "items": []
+
+    "Triggered":
+    """oh hey! You’re the hot guy from last night. I have no idea what happened to you last night, all I know is that you went out the back afterwards; with some others to go get a kebab I think """
+
+    "memory": []
+
+
+}
+
+friend_Kebab_Worker = {
+
+    "name": "Kebabby"
+    
+    "items": []
+
+    "Triggered":
+    """he mentioned you and a woman being here last night.  """
+
+    "memory": []    
+
+
+
+}

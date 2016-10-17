9141603990e86891a867af2b374d74e8fa909242
Trimbae <matthewvtrimby@gmail.com>
2016-10-17 10:38:42 +0100

    Updated name of the taf

--- map.py
+++ map.py
@@ -6,7 +6,7 @@ room_flat = {
     "description":
     """You awaken in a horrid state, to find that the room around you is littered with rubbish and debris. It feels like a distant dream to you now. You stumble to your feet clutching your head with your hand. Struggling to re-call of what had happened the night before. But nothing comes to fruition. You hear a bestial growl. Upon looking toward, the direction of the noise. Your eyes stop at the table. You notice that your friend Rick is laying there hungover, with a bottle of Budweiser in his hand and drool hanging from his mouth.
     Your stomach rumbles in accordance to the consumption of alcohol. You stare blankly around the room and at the kitchen area.For answers as to what has happened and what to do.""",
-    
+
     "exits": {"south": "Mortys"},
 
     "items": [item_bacon,]
@@ -21,14 +21,14 @@ room_Mortys = {
 
     "description_alt":
     """Upon reaching Morty’s house, you knock on the door and wait a couple of minutes. You hear nothing except for the voice in your head saying one thing… “Where is he?” … upon waiting for a sign nothing is happening and your starting to look strange standing outside your friend’s house for a while now whilst still half hung over. """,
-    
+
     "exits":  {"north": "flat", "west":"The_Taf"},
 
     "items": []
 }
 
 room_taf = {
-    "name": "the_taf",
+    "name": "the taf",
 
     "description":
     """You enter the Taf, with pride in feeling like you totally owned this place last night. Although a breeze of shame seems to have crawled up your spine. Walking now towards the bar you notice a member of staff just stood there, you walk up and ask them “Have you seen my jacket around here?”.
@@ -47,7 +47,7 @@ room_PRYZM = {
 
     "description":
     """Upon walking up to the club in your jacket looking the most boss you can be, turning to what feels like an adoring crowd as you walk down the road. When…really theres not much of a crowd… it’s just the self-satisfaction of how well you’re doing going to your head… and you probably look like a pretentious dick to some of them. Just saying how it is… Anyway.
-    You approach the bouncer; you raise your hand for a fist bump. He fist bumps as you raise it to him. You then say to him, “aaah bouncer you druggo, how you doing?”. To which he replies,” nah yeah, yeah nah. Doing great mate. Just been working all night, you know?”. You then utter to him, “yeah man, I hear ya’. Worlds tough you know?”. He then smacks you in the back with approval, “Mate you know exactly! look you seem busy… so you can get in for free this time yeah?”. In appreciation, you humbly say “Mate you are a legend, I owe you one!”. He turns to t1he cashier, “Sally let this bloke in, his alright”.  She nods with a sigh. 
+    You approach the bouncer; you raise your hand for a fist bump. He fist bumps as you raise it to him. You then say to him, “aaah bouncer you druggo, how you doing?”. To which he replies,” nah yeah, yeah nah. Doing great mate. Just been working all night, you know?”. You then utter to him, “yeah man, I hear ya’. Worlds tough you know?”. He then smacks you in the back with approval, “Mate you know exactly! look you seem busy… so you can get in for free this time yeah?”. In appreciation, you humbly say “Mate you are a legend, I owe you one!”. He turns to t1he cashier, “Sally let this bloke in, his alright”.  She nods with a sigh.
     You carry on in, as you get to the main room. There is a woman sweeping up glass from the floor.""",
 
     "description_alt":
@@ -63,7 +63,7 @@ room_kebab = {
 
     "description":
     """You walk into the kebab shop… prepared and ready for anything… hell your feeling pretty invincible by this point. Like you could probably wrestle a honey badger with no issue… but mate in fairness that’s pretty messed up… wanting to fight animals like that… you should feel ashamed. Anyway you see the crowd of hungry people waiting for their precious fast food. But by this point you don’t care and have the ingenious idea of distracting them so you get a chance to talk to the guy behind the counter.
-    You take in a deep breath… your palms are quite sweaty… knees weak… arms are heavy… still a little bit of vomit on your shirt…. Maybe from mum’s spaghetti?? And finally shout with a might roar. “LOOK IT’S THE NEW MEMBER OF THE BAND GORILLAZ!!! ITS HARAMBE!!! HE IS WALKING DOWN THE STREET OMFG!?”. With a sheer look of confusion from what may as well be called a flock of sheep. Stampede out of the shop to go take a look and try to follow who they assume is the guy. 
+    You take in a deep breath… your palms are quite sweaty… knees weak… arms are heavy… still a little bit of vomit on your shirt…. Maybe from mum’s spaghetti?? And finally shout with a might roar. “LOOK IT’S THE NEW MEMBER OF THE BAND GORILLAZ!!! ITS HARAMBE!!! HE IS WALKING DOWN THE STREET OMFG!?”. With a sheer look of confusion from what may as well be called a flock of sheep. Stampede out of the shop to go take a look and try to follow who they assume is the guy.
     You suavely walk over to the counter, lean over the counter. The shop keeper is looking at you pretty annoyed… well you did just get rid of all his customers… what did you expect to happen? To keep this PG, he started swearing at you with the odd sentence of blaming his wife for choosing such a crazy place to set up shop.""",
 
     "description alt":
@@ -78,14 +78,14 @@ room_summers = {
     "name": "Summers",
 
     "description":
-    """You walk up to Summer’s place… feeling pretty good still, the sun is shining, the bees are buzzing. There is a helicopter flying over your head… maybe a military one? Who knows. All you know is that today’s going pretty swell for an adventure. You get to her door and see a note; it reads “Hung Over… don’t piss me off”. Having a quick think about how she has acted in the past in this situation, you wittedly go rummaging through your pockets and pull out a can of RedBull and pull a flower from her garden. 
-    You then stand braced in front of her door, with both items acting like a sword and shield; ready to take on the dragon that’s in hey lair. You then take a brave step forward and with a valiant lunge you press the doorbell ringing it multiple times. Congratulations… you have now gained a new item… it’s called man hood. After hearing what could potentially be the start of the apocalypse to you, the noise stops and the door slightly creeks open. You see an eye appear which is surrounded by hair, it peers down to the items in your hands. The door swiftly opens and Summer takes the Redbull from your hand. 
-    She then downs the RedBull like she needs it to live. And then throws the empty can into the next room. Hastily then turns back to face you. “So what’s up then?” she says casually. To which you reply, “can you remember what happened last night and if I did anything stupid specifically that stood out?”. She sarcastically replies, “But your stupid all the time” with a cheeky look on her face. You glare at her annoyed. After a few minutes passing of casual pleasantries… trying to keep things pg here people… don’t judge me… she closes the door and you must take the final path on your epic adventure! ON WARDS BRAVE ADVENTURER!… just mind arrows and getting hit in the knee… its not fun trust… 
+    """You walk up to Summer’s place… feeling pretty good still, the sun is shining, the bees are buzzing. There is a helicopter flying over your head… maybe a military one? Who knows. All you know is that today’s going pretty swell for an adventure. You get to her door and see a note; it reads “Hung Over… don’t piss me off”. Having a quick think about how she has acted in the past in this situation, you wittedly go rummaging through your pockets and pull out a can of RedBull and pull a flower from her garden.
+    You then stand braced in front of her door, with both items acting like a sword and shield; ready to take on the dragon that’s in hey lair. You then take a brave step forward and with a valiant lunge you press the doorbell ringing it multiple times. Congratulations… you have now gained a new item… it’s called man hood. After hearing what could potentially be the start of the apocalypse to you, the noise stops and the door slightly creeks open. You see an eye appear which is surrounded by hair, it peers down to the items in your hands. The door swiftly opens and Summer takes the Redbull from your hand.
+    She then downs the RedBull like she needs it to live. And then throws the empty can into the next room. Hastily then turns back to face you. “So what’s up then?” she says casually. To which you reply, “can you remember what happened last night and if I did anything stupid specifically that stood out?”. She sarcastically replies, “But your stupid all the time” with a cheeky look on her face. You glare at her annoyed. After a few minutes passing of casual pleasantries… trying to keep things pg here people… don’t judge me… she closes the door and you must take the final path on your epic adventure! ON WARDS BRAVE ADVENTURER!… just mind arrows and getting hit in the knee… its not fun trust…
     """,
 
     "description alt":
-    """You walk to Summer’s place after such a long distance and seeing many sights and hearing many sounds, you feel like your day could really have gone better to begin with. But hey things could have been worse right? So you approach the door to Summer’s place, about to knock on the door. When you notice a note reading, “Hung over… don’t piss me off”. Your memory kicks in of previous times to when she has been angry… like the time you pissed on her door step because you hated her next door neighbour and tried blaming it on them… when in reality doing that when you were drunk… probably not the most of helpful things in your quest. 
-    So now you stand outside another person’s house…. Looking strange as you contemplate over whether it’s a good idea to disturb her. You are really out to make friends today aren’t you? 
+    """You walk to Summer’s place after such a long distance and seeing many sights and hearing many sounds, you feel like your day could really have gone better to begin with. But hey things could have been worse right? So you approach the door to Summer’s place, about to knock on the door. When you notice a note reading, “Hung over… don’t piss me off”. Your memory kicks in of previous times to when she has been angry… like the time you pissed on her door step because you hated her next door neighbour and tried blaming it on them… when in reality doing that when you were drunk… probably not the most of helpful things in your quest.
+    So now you stand outside another person’s house…. Looking strange as you contemplate over whether it’s a good idea to disturb her. You are really out to make friends today aren’t you?
     """,
 
     "exits": {"north": "Kebab_Shop","west":"End"},
@@ -98,7 +98,7 @@ room_end = {
 
     "description":
     """You run through your door with a massive smile on your face and feeling rather accomplished. Bravo you clever little sausage. Rick turns his head from the TV in confusement; then shrugs his shoulders and says, “mate just remembered that your keys are in the kebab box in the bin”. And he then turns back to the TV.  You then drop your noble pose and face-palm, then slowly walk over to the now less than sanitary kitchen. You then walk over to the bin and Jab your finger at its lid. As you do so, you can smell the horrid remains linger from its container. It sends your nostrils into disarray; which causes you to leap backwards. You grab a peg from among the random objects in the kitchen and put it on your nose. You then, quickly turn your side to the bin and shove your hand straight into it without looking to see if you can feel the kebab box.
-    After a couple of seconds of searching, you feel what appears to be the box at the bottom you pull it out with full force and even let out a little grunt. Kind of imagine a warrior that’s just ripped out the heart of his foe with his hand… only it’s you… and well… its rubbish in your hand. Rick turns and looks at your accomplishment and slowly claps and subtly says, “Mate, you need to calm down… like have a pint or something” then he just goes back to watching TV. You open the box and grab the key which is covered in kebab sauce and other contents of the bin. You hurry to the sink like your life depends on it and wash off the waste. You then walk over to your bedroom door and unlock it. As your key turns in the lock, Rick is screaming “GOOOOAAAL!” As Manchester United scored against Liverpool. To which you afterwards then step through the door and close it behind you and notice a video camera sat on the bed still recording. You take the camera into the next room where Rick is sat. 
+    After a couple of seconds of searching, you feel what appears to be the box at the bottom you pull it out with full force and even let out a little grunt. Kind of imagine a warrior that’s just ripped out the heart of his foe with his hand… only it’s you… and well… its rubbish in your hand. Rick turns and looks at your accomplishment and slowly claps and subtly says, “Mate, you need to calm down… like have a pint or something” then he just goes back to watching TV. You open the box and grab the key which is covered in kebab sauce and other contents of the bin. You hurry to the sink like your life depends on it and wash off the waste. You then walk over to your bedroom door and unlock it. As your key turns in the lock, Rick is screaming “GOOOOAAAL!” As Manchester United scored against Liverpool. To which you afterwards then step through the door and close it behind you and notice a video camera sat on the bed still recording. You take the camera into the next room where Rick is sat.
     And you tell him, “mate want to see what actually happened last night?”. He grabs the remote for the TV and turns it off and shouts, “Lets do this!”.""",
 
     "exits": {"east":"Summers", "north": "End"},
@@ -115,5 +115,5 @@ rooms = {
     "Kebab_Shop": room_kebab,
     'Summers': room_summers,
     "End": room_end,
-    
+
 }
